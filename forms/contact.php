7e3e2ee0c9ba1cfeb9d159c30df495edc8b0af4f
nehal <134355098+nr-dev24@users.noreply.github.com>
2024-05-11 20:22:50 +0530

    Update form

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,9 @@
-<?php
+/*<?php
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+//  $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+/*  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -27,9 +27,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
+/*  $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+  echo $contact->send();*/
+?>*/
